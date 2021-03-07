89e630b3c8f8f9d055e34f6b1edc1d2294676948
Kamil Dabek <dabekk@tcd.ie>
2021-03-07 16:42:32 +0000

    Update s3_log_parser.py
    - optimized search for RIDs with Regex
    - included user arguments with argparse (input log file and output file for RIDs)
    - Stopped using output redirect ---> writing to file now with f.write()
    - added some cosmetic output separation e.g. line breaks etc.
    - now using os library to give user feedback regarding which file program is analysing and where it will output the RIDs (full filepaths)

--- Parser/s3_log_parser.py
+++ Parser/s3_log_parser.py
@@ -2,36 +2,41 @@
 Author: Kamil Dabek
 Program will extract the S3 request IDs from a log file of an S3 request
 Input: filepath to an S3 log file
-Output: S3 request IDs from log file
+Output: S3 request IDs from log file into a separate text file
+
 Additional note: to extract S3 request ID via CLI. Run command with --debug flag e.g. aws s3 ls --debug 2> s3_log.txt
 """
-import sys
 
-FILEPATH = '/path/to/file/s3_log_example.txt'
-REQUEST_ID_SHORT = "x-amz-request-id"
-REQUEST_ID_LONG = "x-amz-id-2"
+import os
+import argparse
+import re
 
-def parse_s3_log(filepath):
-    print(f'Log at: {filepath}')
-    log_lines = []
-    with open(filepath) as s3_logs:
-        log_lines = s3_logs.readlines()
+parser = argparse.ArgumentParser(description='Will extract the S3 Request ID pairs from an S3 request debug file and export the IDs to a new file.')
+parser.add_argument('-i', '--inputfile', type=str, metavar='', required=True, help='file where the S3 debug logs are located')
+parser.add_argument('-o', '--outputfile', type=str, metavar='', default="request_ids1.txt", help='file where you want the extracted S3 IDs to be written')
+args = parser.parse_args()
+
+INPUTFILE = args.inputfile
+OUTPUTFILE = args.outputfile
+# request ID tags have ': ' at the end to skip the useless characters before the RID
+REQUEST_ID_SHORT = "x-amz-request-id': '"
+REQUEST_ID_LONG = "x-amz-id-2': '"
+
+def parse_s3_log():
+    print(f'Getting request IDs from: {os.path.abspath(INPUTFILE)} and outputting to {os.path.abspath(OUTPUTFILE)}')
+    with open(INPUTFILE, 'r') as s3_logs:
 
     # request ID pairs will be written to external file request_ids.txt
-    with open('request_ids.txt', 'w') as f:
-        sys.stdout = f
-        for line in log_lines:
-            if REQUEST_ID_SHORT in line:
-                id_index = line.find(REQUEST_ID_SHORT)  # get the index where REQUEST_ID_SHORT parameter begins
-                # extracts the value for REQUEST_ID_SHORT (hardcoding +4/-4 to extract only the ID)
-                short_id = line[len(REQUEST_ID_SHORT) + id_index + 4: line.rfind('Date') - 4]
-                print(f'short request ID: {short_id}')
+        with open(OUTPUTFILE, 'w', newline="\n") as f:
+            f.write(f"S3 Request IDs from file in {os.path.abspath(INPUTFILE)}\n----------------------\n")
+            for line in s3_logs:
+                if bool(re.search(f'{REQUEST_ID_SHORT}', f'{line}')):   # use regex to check if RID contained in line
+                    short_id = re.split(f'\\b{REQUEST_ID_SHORT}\\b', line)[1][0:16]     # takes first 16 chars after RID tag
+                    f.write(f"""short request ID: {short_id}\n""")
 
-            if REQUEST_ID_LONG in line:
-                id_index = line.find(REQUEST_ID_LONG)  # get the index where REQUEST_ID_LONG parameter begins
-                # extracts the value for REQUEST_ID_LONG (hardcoding +4/-4 to extract only the ID)
-                long_id = line[len(REQUEST_ID_LONG) + id_index + 4: line.rfind('x-amz-request-id') - 4]
-                print(f'long request ID: {long_id}')
+                if bool(re.search(f'{REQUEST_ID_LONG}', f'{line}\n')):   # use regex to check if RID contained in line
+                    long_id = re.split(f'\\b{REQUEST_ID_LONG}\\b', line)[1][0:76]     # takes first 76 chars after RID tag
+                    f.write(f"""long request ID:  {long_id}\n""")
+                    f.write("----------------------\n")
 
-if __name__ == '__main__':
-    parse_s3_log(filepath=FILEPATH)
+parse_s3_log()      # calls log parser function
